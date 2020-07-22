ded212f69f00c190cc251659a861782201171f75
Ashish Magadum <shonenash@gmail.com>
2020-07-22 20:26:38 +0530

    UC_DSA_C2_P1

---
+++ UC_DSA/data_structures/p1_check_brackets_[STACK].py
@@ -0,0 +1,73 @@
+class Item:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.start = None
+
+    def push(self, value):
+        x = Item(value)
+        if self.start is None:
+            self.start = x
+        else:
+            x.next = self.start
+            self.start = x
+        del x
+
+    def pop(self):
+        if self.start is None:
+            # print("Stack Underflow!")
+            return None
+        else:
+            x = self.start
+            val = x.value
+            self.start = self.start.next
+            del x
+            return val
+
+    def peek(self):
+        if self.start is None:
+            # print("Stack is Empty!")
+            return None
+        else:
+            return self.start.value
+
+
+def are_matching(left, right):
+    return (left + right) in ["[]", "{}", "()"]
+
+
+def main():
+    st = Stack()
+    str = input()
+    last_brk = [-1, None]
+
+    for i, c in enumerate(str):
+        if c in "[{(":
+            last_brk = list([i, c])
+            st.push(list([i, c]))
+        elif c in "]})":
+            popped = st.pop()
+            if popped is None:
+                print(i + 1)
+                return
+            if not are_matching(popped[1], c):
+                print(i + 1)
+                return
+            else:
+                if st.peek() is not None:
+                    last_brk = st.peek()
+                else:
+                    last_brk = [-1, None]
+
+    if last_brk[0] != -1 and st.start is not None:
+        print(last_brk[0] + 1)
+    else:
+        print("Success")
+
+
+if __name__ == '__main__':
+    main()
